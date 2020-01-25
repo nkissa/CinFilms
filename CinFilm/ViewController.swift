81cbcf28566eec9cbf5227aca3dd4cdcdfca0e19
Nasar Issa <nkissa@ucsd.edu>
2020-01-24 16:50:42 -0800

    Initial Commit

---
+++ CinFilm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CinFilm
+//
+//  Created by Nasar K Issa on 1/24/20.
+//  Copyright © 2020 Nasar K Issa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
