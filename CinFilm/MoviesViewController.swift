6739cbb65618b823dcf2a1c1d96c64c947832252
Nasar Issa <nkissa@ucsd.edu>
2020-01-24 17:12:26 -0800

    Added TableCell into app

--- CinFilm/MoviesViewController.swift
+++ CinFilm/MoviesViewController.swift
@@ -8,12 +8,46 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    var movies = [[String:Any]]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello")
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+              print(dataDictionary)
+            
+            self.movies = dataDictionary["results"] as! [[String:Any]]
+              // TODO: Get the array of movies
+              // TODO: Store the movies in a property to use elsewhere
+              // TODO: Reload your table view data
 
+           }
+        }
+        task.resume()
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "row: \(indexPath.row)"
+        return cell
     }
     
 
