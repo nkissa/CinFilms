bcbe731025aee353dd7db7375d48632ef4387c98
Nasar Issa <nkissa@ucsd.edu>
2020-01-27 17:06:22 -0800

    Added Custom Cells into CinFilm

---
+++ CinFilm/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  CinFilm
+//
+//  Created by Nasar K Issa on 1/27/20.
+//  Copyright © 2020 Nasar K Issa. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
