9ce6bf9754696db44d7ea4750620c393b770b6d2
Nasar Issa <nkissa@ucsd.edu>
2020-01-30 19:06:06 -0800

    Added a Table Bar Controller and Tree Structure

---
+++ CinFilm/MovieGridViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieGridViewController.swift
+//  CinFilm
+//
+//  Created by Nasar K Issa on 1/30/20.
+//  Copyright © 2020 Nasar K Issa. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
